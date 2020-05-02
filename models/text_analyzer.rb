fc2e30c6c6a944433ef72ef0b4ff63dfb30c05f7
vanessuniq <vanessuniq@gmail.com>
2020-05-02 19:45:56 -0400

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,38 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+    attr_reader :text
+
+    def initialize(text)
+        @text = text.downcase
+    end
+
+    def count_of_words
+        words = text.scan(/[a-z]+/)
+        words.count
+    end
+
+    def count_of_vowels
+        text.scan(/[aeoui]/).count
+    end
+
+    def count_of_consonants
+        text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    end
+
+    def most_used_letter
+        s = text.gsub(/[^a-z]/, '').split('')
+        h = {}
+        s.each do |w|
+            if h[w]
+                h[w] += 1
+            else
+                h[w] = 1
+            end
+        end
+        
+        h.select do |k|
+            h[k] == h.values.max
+        end
+    end
+    
+end
